48240f1a2a06d9879a8c31c77a525944410808ea
Banibrata Manna <mbanibrata668@gmail.com>
2024-11-05 22:47:51 +0530

    Creating Simple Todo Rest Api

---
+++ src/main/java/com/in28minutes/learn_spring_security/resources/TodoResource.java
@@ -0,0 +1,37 @@
+package com.in28minutes.learn_spring_security.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoResource {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private static final List<Todo> TODOS_LIST = List.of(new Todo("Banibrata", "Learn Spring Security"),
+			new Todo("Banibrata", "Make College Placement Cell"));
+
+	@GetMapping("/todos")
+	public List<Todo> retrieveAllTodos() {
+		return TODOS_LIST;
+	}
+	@GetMapping("/users/{username}/todos")
+	public Todo retirieveTodosForSpecificUser(@PathVariable("username") String username) {
+		return TODOS_LIST.get(1);
+	}
+	@PostMapping("/users/{username}/todos")
+	public void createTodoforSpecificUser(@PathVariable("username") String username,
+			@RequestBody Todo todo) {
+		logger.info("Create {} for {}", todo, username);
+		TODOS_LIST.add(todo);
+	}
+}
+
+record Todo(String uername, String description) {}
