884631d717d1348706cd69fbfeed9215e5bd9acc
Banibrata Manna <mbanibrata668@gmail.com>
2024-11-06 15:27:00 +0530

    Setting up JWT
    Setting up RSA key pair and it's generator and source for private and public key, for Decoder and encoder

--- src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,8 +1,12 @@
-package com.in28minutes.learn_spring_security.basic;
+package com.in28minutes.learn_spring_security.jwt;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import java.net.PasswordAuthentication;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 import javax.sql.DataSource;
 
@@ -11,16 +15,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+@Configuration
+public class JwtSecurityConfiguration {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(
@@ -36,6 +51,8 @@ public class BasicAuthSecurityConfiguration {
 		http.httpBasic(withDefaults());
 		http.csrf().disable();
 		http.headers().frameOptions().sameOrigin();
+		
+		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		return http.build();
 	}
 //	@Bean
@@ -85,5 +102,37 @@ public class BasicAuthSecurityConfiguration {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder () {
 		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public KeyPair keyPair() {
+		try {
+			var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			throw new  RuntimeException();
+		}
+	}
+	@Bean
+	public RSAKey rsaKey(KeyPair keyPair) {
+		return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+			.privateKey(keyPair.getPrivate())
+			.keyID(UUID.randomUUID().toString())
+			.build();	
+	}
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+		var jwkSet = new JWKSet(rsaKey);
+		return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+	}
+	@Bean
+	public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+		return NimbusJwtDecoder
+				.withPublicKey(rsaKey.toRSAPublicKey())
+				.build();
+	}
+	@Bean
+	public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+		return new NimbusJwtEncoder(jwkSource);
 	}
 }
